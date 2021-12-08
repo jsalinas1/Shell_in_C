789935da574ffe2807400f6e79199b1f7d805d95
Jezreel Salinas <jsalinas@cpp.edu>
2021-12-08 00:14:34 -0800

    Added lsh_execute() function

--- main.c
+++ main.c
@@ -165,3 +165,14 @@ int lsh_help(char **args){
 int lsh_exit(char **args){
     return 0;
 }
+
+int lsh_execute(char **args){
+    if(args[0] == NULL)
+        return 1;
+
+    for(int i = 0; i < lsh_numbuiltins(); i++)
+        if(strcmp(args[0], builtin_str[i]) == 0)
+            return (*builtin_func[i])(args);
+    
+    return lsh_launch(args);
+}
