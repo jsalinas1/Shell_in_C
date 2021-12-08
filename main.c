729b40da322bde4ea07d9b88478e8bf6a15f08c0
Jezreel Salinas <jsalinas@cpp.edu>
2021-12-07 23:44:52 -0800

    Initialized main

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char **argv){
+    lsh_loop();
+    
+    return EXIT_SUCCESS;
+}
