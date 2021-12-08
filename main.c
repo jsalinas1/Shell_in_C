0e57ad85fef4f52c6c61dd2a6a78ddc84be8b261
Jezreel Salinas <jsalinas@cpp.edu>
2021-12-07 23:46:49 -0800

    Added lsh_loop() function and implemented statements

--- main.c
+++ main.c
@@ -6,6 +6,20 @@
 
 int main(int argc, char **argv){
     lsh_loop();
-    
     return EXIT_SUCCESS;
+}
+
+void lsh_loop(void){
+    char *line;
+    char **args;
+    int status;
+
+    do{
+        printf("> ");
+        line = lsh_read_line();
+        args = lsh_split_line(line);
+
+        free(line);
+        free(args);
+    }while(status);
 }
